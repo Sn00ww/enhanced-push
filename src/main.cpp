311533b5f866c37bf3659eb7224882cf4a9f3e4f
Sn0w <clement.ozor@protonmail.com>
2022-07-18 16:36:54 +0200

    [develop] => Add possibility to push without commit with the flag -P

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include "push.hpp"
 #include "Arguments.hpp"
 
+void printHelp(char *command)
+{
+    std::cout << "Usage:" << std::endl;
+    std::cout << command << " -m <message> [-a] [-p]" << std::endl;
+    std::cout << "\nOptions:" << std::endl;
+    std::cout << "\t-m <message>: Specify the commit message (not formatted)." << std::endl;
+    std::cout << "\t-a: Track all the files (git add .)." << std::endl;
+    std::cout << "\t-p: Push after the commit." << std::endl;
+    std::cout << "\t-P: Push without commit." << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
     Utils::Arguments args(argc, argv);
 
+    if (args.isSet("-h") || args.isSet("--help")) {
+        printHelp(argv[0]);
+        return 0;
+    }
+
+    if (args.isSet("-P")) {
+        system("git push");
+        return 0;
+    }
+
     if (!args.isSet("-m") || args.getArgIndex("-m") + 1 >= args.getArgc()) {
-        std::cout << "Usage: " << argv[0] << " -m <message> [-a] [-p]" << std::endl;
+        printHelp(argv[0]);
         return 1;
     }
 
